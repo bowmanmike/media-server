20f984480c72615ff10291251926941cadcf76a6
Mike Bowman <mike@mikebowman.dev>
2025-11-25 20:16:30 -0500

    Add logging to transmission

--- transmission/on-complete.sh
+++ transmission/on-complete.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
+set -euo pipefail
 
+echo "Transmission download completed. Notifying organizer to scan."
+echo "Transmission info: torrent_dir=$TR_TORRENT_DIR, torrent_name=$TR_TORRENT_NAME"
 curl -s -X POST http://organizer:8000/scan-once >/dev/null 2>&1
+
+echo "Notified"
